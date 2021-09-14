e9220e5ca912d7ef86119f77259a668d98a61532
aswini devi <aaswinidevi@gmail.com>
2021-09-14 20:51:27 +0530

    first commit

--- list.js
+++ list.js
@@ -4,24 +4,21 @@ function item() {
   xhttp.onreadystatechange = function() {
         if(this.readyState==4&& this.status==200){
      var response = JSON.parse(this.responseText);
-   var list = response.data;
+   var list = response.list;
    var output="";
    for(  var i=0; i<list.length; i++)
    {
        output+=`<tr>
-                   <td>${list[i].SlNo}</td>
-                   <td>${list[i].ItemName}</td>
+                   <td>${list[i].serialnumber}</td>
+                   <td>${list[i].Name}</td>
                    <td>${list[i].Quantity}</td>
                    <td>${list[i].Unit}</td>
                    <td>${list[i].Department}</td>
-                   <td>${list[i].Notes}</td>
+                   <td>${list[i].Note}</td>
                </tr>`
-      table.innerHTML += output;
+    
    }
-  //  document.getElementById("tdata").innerHTML = output;
-
-
-
+   document.getElementById("tdata").innerHTML = output;
     }
    }
   xhttp.open("GET", "list.json", true);
